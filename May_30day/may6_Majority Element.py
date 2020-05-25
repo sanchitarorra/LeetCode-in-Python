6d531b0c7497c27ce58116b2ffb6f47744f41520
sanchitarorra <sanchit.arora41@gmail.com>
2020-05-25 15:25:34 +0530

    may_06 solution

---
+++ May_30day/may6_Majority Element.py
@@ -0,0 +1,10 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        dict = {}
+        for num in nums:
+            if num not in dict:
+                dict[num] = 1
+            else:
+                dict[num] += 1
+            if dict[num] > len(nums) // 2:
+                return num
