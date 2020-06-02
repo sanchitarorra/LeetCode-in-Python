c334686031550aed0f1afcc08bcc9a133d9ffe76
sanchitarorra <sanchit.arora41@gmail.com>
2020-06-03 01:42:46 +0530

    june02

---
+++ June_30day/june02_Delete Node in a Linked List.py
@@ -0,0 +1,10 @@
+#Time = O(1) and space = O(1)
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
+        
